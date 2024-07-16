677fe9a23aa4612ae00dca2aab40685a3f63a564
KSaMar <baobao_jk@qq.com>
2024-07-16 14:06:30 +0800

    upload modules

---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/home/IndexView.vue'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    }
+  ]
+})
+
+export default router
